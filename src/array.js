45ccb5eb509a3a7870bfe21e089e882684871815
Scott Davis <scott@thirstyhead.com>
2016-05-10 10:16:02 -0600

    added array.js

---
+++ src/array.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let statusCodes = ['ok', 'warning', 'OK', 'Error', 'Ok', 'ok', 'OK', 'Warning'];
+
+//TODO: Normalize codes
+//TODO: Only show warnings
+
+let warnings = [];
+for(let i=0; i<statusCodes.length; i++){
+  let code = statusCodes[i].toLowerCase();
+  if(code === 'warning'){
+    warnings.push(code);
+  }
+}
+
+console.log(`All Warnings: ${warnings}`);
